5656da1aea4e720459de700b45e6a1eac6f073d6
Eureka <caesarioarya288@gmail.com>
2024-07-10 01:02:53 +0700

    vercel

--- app/api/v1/auth/router.js
+++ app/api/v1/auth/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express();
 const { signinCms, getUser } = require("./controller");
 
-router.post("/:nama", getUser);
+router.get("/:nama", getUser);
 router.post("/signin", signinCms);
 
 module.exports = router;
